66c53817978458752e257d2fc4366dc6ee10ab5c
us3r1723 <83931551+us3r1723@users.noreply.github.com>
2023-09-27 20:02:12 -0300

    Add files via upload

--- java/Sprint/Sprint.java
+++ java/Sprint/Sprint.java
@@ -1,122 +1,105 @@
 package java_sipi;
-
 import java.util.Scanner;
 
-public class Sprint {
+public class gpt {
 
-    static Scanner entrada = new Scanner(System.in);
-    static int numEqui = 0;
+	    static Scanner entrada = new Scanner(System.in);
+	    static int numEqui = 0 ,ctl2=0;
 
-    public static void main(String[] args) {
-        while (numEqui < 1) {
-            System.out.println("Digite a quantidade de equipes");
-            numEqui = entrada.nextInt();
-        }
+	    public static void main(String[] args) {
+	        while (numEqui < 1) {
+	            System.out.println("Digite a quantidade de equipes");
+	            numEqui = entrada.nextInt();
+	        }
 
-        int[] pontuacao = new int[numEqui];
-        int[] nomeEqui = new int[numEqui];
-        int ctl = 0;
+	        int[] pontuacao = new int[numEqui];
+	        int[] nomeEqui = new int[numEqui];
+	        int ctl = 0;
 
-        int numBat = 0;
-        while (ctl < numEqui) {
-            int ctl2 = 0;
+	        int numBat = 0;
+	        while (ctl < numEqui) {
+	            int ctl2 = 0;
 
-            while (nomeEqui[ctl] > 99 || nomeEqui[ctl] < 11) {
-                System.out.println("Digite o número da " + (ctl + 1) + "ª equipe");
-                nomeEqui[ctl] = entrada.nextInt();
-                if (nomeEqui[ctl] > 99 || nomeEqui[ctl] < 11) {
-                    System.out.println("Valor inválido, tente novamente");
-                    nomeEqui[ctl] = 0;
-                }
-            }
+	            while (nomeEqui[ctl] > 99 || nomeEqui[ctl] < 11) {
+	                System.out.println("Digite o número da " + (ctl + 1) + "ª equipe");
+	                nomeEqui[ctl] = entrada.nextInt();
+	                if (nomeEqui[ctl] > 99 || nomeEqui[ctl] < 11) {
+	                    System.out.println("Valor inválido, tente novamente");
+	                    nomeEqui[ctl] = 0;
+	                }
+	            }
 
-            while (numBat < 1) {
-                System.out.println("Digite a quantidade de batalhas");
-                numBat = entrada.nextInt();
-                if (numBat < 1) {
-                    System.out.println("Inválido");
-                }
-            }
+	            while (numBat < 1) {
+	                System.out.println("Digite a quantidade de batalhas");
+	                numBat = entrada.nextInt();
+	                if (numBat < 1) {
+	                    System.out.println("Inválido");
+	                }
+	            }
 
-            char luta = 'z';
-            while (ctl2 < numBat) {
-                System.out.println("Digite o resultado da " + (ctl2 + 1) + "ª Luta");
-                System.out.println("V - Para Vitória");
-                System.out.println("E - Para Empate");
-                System.out.println("D - Para Derrota");
-                System.out.println("");
-                luta = Character.toUpperCase(entrada.next().charAt(0));
+	            char luta = 'z';
+	            while (ctl2 < numBat) {
+	                System.out.println("Digite o resultado da " + (ctl2 + 1) + "ª Luta");
+	                System.out.println("V - Para Vitória");
+	                System.out.println("E - Para Empate");
+	                System.out.println("D - Para Derrota");
+	                System.out.println("");
+	                luta = Character.toUpperCase(entrada.next().charAt(0));
 
-                if (luta == 'V') {
-                    pontuacao[ctl] += 5;
-                    ctl2++;
-                } else if (luta == 'E') {
-                    pontuacao[ctl] += 3;
-                    ctl2++;
-                } else if (luta == 'D') {
-                    ctl2++;
-                } else {
-                    System.out.println("Inválido");
-                }
-                luta = 'z';
-            }
+	                if (luta == 'V') {
+	                    pontuacao[ctl] += 5;
+	                    ctl2++;
+	                } else if (luta == 'E') {
+	                    pontuacao[ctl] += 3;
+	                    ctl2++;
+	                } else if (luta == 'D') {
+	                    ctl2++;
+	                } else {
+	                    System.out.println("Inválido");
+	                }
+	                luta = 'z';
+	            }
 
-            ctl++;
-        }
+	            ctl++;
+	        }
 
-       
-        System.out.println(" ");
-        System.out.println("Equipe  Pontuação");
-        for (int j = 0; j < numEqui; j++) {
-            
-                System.out.print(pontuacao[j] + "\t");
- 
-        }
+	       
+	        System.out.println(" ");
+	        System.out.println("Equipe  Pontuação");
+	        for (int j = 0; j < numEqui; j++) {
+	            
+	                System.out.print(pontuacao[j] + "\t");
+	 
+	        }
 
-        // Calcular os resultados finais
-        int[][] result = new int[numEqui][2];
-        ctl = 0;
-        int ctl2 = 0;
+	        // Calcular os resultados finais
+	        int[][] result = new int[numEqui][2];
 
-        // Reiniciar ctl e ctl2
-        ctl = 0;
-        ctl2 = 0;
+	        for (int i = 0; i < numEqui; i++) {
+	            result[i][0] = nomeEqui[i];
+	            result[i][1] = pontuacao[i];
+	        }
 
-        int[] copiaPontuacao = pontuacao.clone();
-        int[] copiaNomeEqui = nomeEqui.clone();
-        
+	        // Ordenar os resultados em ordem decrescente de pontuação
+	        for (int i = 0; i < numEqui - 1; i++) {
+	            for (int j = i + 1; j < numEqui; j++) {
+	                if (result[i][1] < result[j][1]) {
+	                    // Troca as equipes de posição
+	                    int tempEquipe = result[i][0];
+	                    int tempPontuacao = result[i][1];
+	                    result[i][0] = result[j][0];
+	                    result[i][1] = result[j][1];
+	                    result[j][0] = tempEquipe;
+	                    result[j][1] = tempPontuacao;
+	                }
+	            }
+	        }
 
-        
-        
-        while (ctl < numEqui) {
-        	int ctl3=0;
-            while (ctl2 < numEqui) {
-                if (result[ctl][1] < copiaPontuacao[ctl2]) {
-                    result[ctl][1] = copiaPontuacao[ctl2];
-                    result[ctl][0] = nomeEqui[ctl2];
-                    pontuacao[ctl2] = 0;
-                    ctl3=ctl2;
-                    System.out.println(result[ctl2]);
-                }
-            
-                
-                ctl2 += 1;
-            }
-            pontuacao[ctl3]=0;
-            copiaPontuacao = pontuacao.clone();
-            
-            ctl2 = 0;
-            ctl++;
-            ctl3=0;
-        }
-        // TA ZERANDO NO LUGAR ERRADO
-        System.out.println(" ");
-        System.out.println("Equipe  Pontuação");
-        for (int j = 0; j < numEqui; j++) {
-            for (int i = 0; i < 2; i++) {
-                System.out.print(result[j][i] + "\t");
-            }
-            System.out.println(" ");
-        }
-    }
+	        // Imprimir os resultados finais
+	        System.out.println(" ");
+	        System.out.println("Equipe  Pontuação");
+	        for (int j = 0; j < numEqui; j++) {
+	            System.out.println(result[j][0] + "\t" + result[j][1]);
+	        }
+	    }
 }
